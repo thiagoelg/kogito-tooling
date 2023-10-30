3e8ad78e95651f90285b26d8bbc5f18208dbd331
Wagner Scholl Lemos <josephblt@gmail.com>
2023-10-30 16:57:35 -0300

    Fixes to the integration tests.

--- packages/kie-sandbox-distribution/tests/integrationTest.test.js
+++ packages/kie-sandbox-distribution/tests/integrationTest.test.js
@@ -4,7 +4,7 @@ const execSync = require("child_process").execSync;
 const env = kieSandboxDistributionEnv.env;
 const kieSandboxUrl = `http://127.0.0.1:${env.kieSandboxDistribution.kieSandbox.exposedPort}`;
 const corsProxyUrl = `http://127.0.0.1:${env.kieSandboxDistribution.corsProxy.exposedPort}`;
-const extendedServicesUrl = `http://127.0.0.1:${env.kieSandboxDistribution.extendedServices.exposedPort}`;
+const extendedServicesUrl = `http://0.0.0.0:${env.kieSandboxDistribution.extendedServices.exposedPort}`;
 
 describe("Test built images individually", async () => {
   it("cors-proxy homepage", async () => {
@@ -16,8 +16,11 @@ describe("Test built images individually", async () => {
   it("extended-services homepage", async () => {
     expect(await (await fetch(extendedServicesUrl)).text()).toMatchSnapshot();
   });
-  it("extended-services ping", async () => {
-    expect(await (await fetch(`${extendedServicesUrl}/ping`)).json()).toHaveProperty("proxy");
+  it("extended-services-java ping", async () => {
+    const response = await (await fetch(`${extendedServicesUrl}/ping`)).json();
+    expect(response).toHaveProperty("version");
+    expect(response).toHaveProperty("started");
+    expect(response.started).toBe(true);
   });
   it("kie-sandbox homepage", async () => {
     expect(await (await fetch(kieSandboxUrl)).text()).toContain('<script src="index.js"></script>');

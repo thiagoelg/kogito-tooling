34cf9c14ae2f2da31e705598de651bcc37dfa661
Thiago Lugli <thiagoelg@gmail.com>
2023-11-03 16:54:28 -0300

    Use 127.0.0.1

--- packages/kie-sandbox-distribution/tests/integrationTest.test.js
+++ packages/kie-sandbox-distribution/tests/integrationTest.test.js
@@ -4,7 +4,7 @@ const execSync = require("child_process").execSync;
 const env = kieSandboxDistributionEnv.env;
 const kieSandboxUrl = `http://127.0.0.1:${env.kieSandboxDistribution.kieSandbox.exposedPort}`;
 const corsProxyUrl = `http://127.0.0.1:${env.kieSandboxDistribution.corsProxy.exposedPort}`;
-const extendedServicesUrl = `http://0.0.0.0:${env.kieSandboxDistribution.extendedServices.exposedPort}`;
+const extendedServicesUrl = `http://127.0.0.1:${env.kieSandboxDistribution.extendedServices.exposedPort}`;
 
 describe("Test built images individually", async () => {
   it("cors-proxy homepage", async () => {

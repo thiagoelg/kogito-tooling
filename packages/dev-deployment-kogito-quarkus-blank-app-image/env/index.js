981e11413cedfd8b677f453f85d1b507e3a69276
Thiago Lugli <thiagoelg@gmail.com>
2024-09-03 16:59:40 -0300

    Ooops

--- packages/dev-deployment-kogito-quarkus-blank-app-image/env/index.js
+++ packages/dev-deployment-kogito-quarkus-blank-app-image/env/index.js
@@ -36,11 +36,11 @@ module.exports = composeEnv([rootEnv], {
       description: "The image used in the FROM import.",
     },
     DEV_DEPLOYMENT_KOGITO_QUARKUS_BLANK_APP_IMAGE__registry: {
-      default: "quay.io",
+      default: "docker.io",
       description: "E.g., `docker.io` or `quay.io`.",
     },
     DEV_DEPLOYMENT_KOGITO_QUARKUS_BLANK_APP_IMAGE__account: {
-      default: "thiagoelg",
+      default: "apache",
       description: "E.g,. `apache` or `kie-tools-bot`",
     },
     DEV_DEPLOYMENT_KOGITO_QUARKUS_BLANK_APP_IMAGE__name: {
